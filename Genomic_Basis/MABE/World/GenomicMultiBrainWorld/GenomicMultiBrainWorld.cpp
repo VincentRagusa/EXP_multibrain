2e5d1c37639762354cb206830e29e25d3b239925
Vincent Ragusa <vincent.ragusa.94@gmail.com>
2020-01-07 16:58:09 -0500

    added the 3 genomic games. some may still need to be tuned to have the desired properties.

--- Genomic_Basis/MABE/World/GenomicMultiBrainWorld/GenomicMultiBrainWorld.cpp
+++ Genomic_Basis/MABE/World/GenomicMultiBrainWorld/GenomicMultiBrainWorld.cpp
@@ -12,17 +12,23 @@
 #include "../../Genome/CircularGenome/CircularGenome.h"
 
 std::shared_ptr<ParameterLink<std::string>>
-GenomicMultiBrainWorld::groupNamePL = Parameters::register_parameter("WORLD_TEST_NAMES-groupNameSpace", (std::string) "root::", "namespace of group to be evaluated");
+GenomicMultiBrainWorld::groupNamePL = Parameters::register_parameter("WORLD_GMB_NAMES-groupNameSpace", (std::string) "root::", "namespace of group to be evaluated");
 
 std::shared_ptr<ParameterLink<std::string>>
-GenomicMultiBrainWorld::genomeNamePL = Parameters::register_parameter( "WORLD_TEST_NAMES-genomeNameSpace", (std::string) "root::", "namespace for parameters used to define brain");
+GenomicMultiBrainWorld::genomeNamePL = Parameters::register_parameter( "WORLD_GMB_NAMES-genomeNameSpace", (std::string) "root::", "namespace for parameters used to define brain");
+
+std::shared_ptr<ParameterLink<int>>
+GenomicMultiBrainWorld::gameCodePL = Parameters::register_parameter( "WORLD_GMB-gameCode", (int) 0, "it picks the game yo");
+
 
 GenomicMultiBrainWorld::GenomicMultiBrainWorld(std::shared_ptr<ParametersTable> PT_):
   AbstractWorld(PT_) {
+  gameCode = gameCodePL->get(PT);
+
   // columns to be added to ave file
   popFileColumns.clear();
   popFileColumns.push_back("score");
-}
+  }
 
 void
 GenomicMultiBrainWorld::evaluateSolo(std::shared_ptr<Organism> org, int analyze, int visualize, int debug) {
@@ -33,9 +39,30 @@ GenomicMultiBrainWorld::evaluateSolo(std::shared_ptr<Organism> org, int analyze,
     std::cout << "GARBO set your genome site type to int ya fool (not really i'm the fool)" << std::endl;
     exit(1);
   }
-
-  for (auto site:genome->sites){
-    score += 100 - std::abs(100 - site);
+  if (gameCode == 0){
+    for (auto site:genome->sites){
+      score += 100 - std::abs(100 - site); //100 is the target site value, assumed to be within the alphabet size
+    }
+  }
+  else if (gameCode == 1){
+    std::fill(site_symbol_counts.begin(), site_symbol_counts.end(), 0);
+    for (auto site:genome->sites){
+      ++site_symbol_counts[site];
+    }
+    auto max = *std::max_element(site_symbol_counts.begin(), site_symbol_counts.end());
+    score = 5000 - max; //5000 is the assumed genome length
+  }
+  else if (gameCode == 2){
+    auto mean = std::accumulate(genome->sites.begin(), genome->sites.end(), 0.0) / genome->sites.size();
+    double variance = 0;
+    for (auto site:genome->sites){
+      variance += std::pow(site-mean, 2);
+    }
+    variance /= genome->sites.size();
+    // -------------
+    // score = (std::sqrt(variance) * std::sin(std::sin(mean)*variance)) + (mean/2); //max score should be 256 +/- a bit for veering off into variance land
+    // (√x)*sin(sin(y)*x) + y/2
+    score = (4*std::tanh(variance) * std::sin(std::sin(mean)*variance)) + (mean/2);
   }
   
   org->dataMap.append("score", score);
@@ -51,10 +78,7 @@ GenomicMultiBrainWorld::evaluate(std::map<std::string, std::shared_ptr<Group>> &
 
 std::unordered_map<std::string, std::unordered_set<std::string>>
 GenomicMultiBrainWorld::requiredGroups() {
-  // return {{groupNamePL->get(PT), {"B:" + genomeNamePL->get(PT) + ",1," + std::to_string(numberOfOutputsPL->get(PT))}}};
   return {{groupNamePL->get(PT), {"G:" + genomeNamePL->get(PT)}}};
-  // requires a root group and a brain (in root namespace) and no additional genome,
-  // the brain must have 1 input, and the variable numberOfOutputs outputs
 }
 
 
